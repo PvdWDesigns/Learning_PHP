12fe336c20cf449df5375b0e86f35b26c09133c7
Petrie <PvdWDesigns@users.noreply.github.com>
2018-12-26 12:46:33 +0200

    some learnings

---
+++ testhtml.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <title>Simple HTML</title>
+    <link type="text/css" rel = "stylesheet" href=".\css\myPHPStyle.css"
+	
+</head>
+<body> <h1>This is HTML</h1>
+    <?php 
+    print'<span class="error">Hello World!</span>';
+    print"line1 \n Line2";
+    ?>
+     <p>More HTML</p>
+</body>
+</html>
