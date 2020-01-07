775bab52a092446acd4f101238bba8cafca94c28
Petrie <PvdWDesigns@users.noreply.github.com>
2020-01-07 16:07:15 +0200

    Update SQLCon.php

--- SQLConnection/SQLCon.php
+++ SQLConnection/SQLCon.php
@@ -1,15 +1,21 @@
+
 <?php
+//phpinfo();
 /*echo "Today is " . date("Y/m/d H:i:s") . "<br>";
 echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l");
 */
-
-$serverName = "localhost"; //serverName\instanceName
+echo getenv("username");
+echo "<br />";
+echo get_current_user();
+echo "<br />";
+echo "<br />";
+$serverName = "didata.cxue5b7z2kgq.eu-west-1.rds.amazonaws.com"; //serverName\instanceName
 
 // Since UID and PWD are not specified in the $connectionInfo array,
 // The connection will be attempted using Windows Authentication.
-$connectionInfo = array( "Database"=>"php_test");
+$connectionInfo = array( "Database"=>"ddassets","UID"=> "ddsecurity", "PWD" =>"AWSddS3cur!ty");
 
 $conn = sqlsrv_connect( $serverName, $connectionInfo); //Try connection
 
@@ -21,7 +27,7 @@ if( $conn ) {
 }
 
 //Insert Query
-$PeopleList = array("Jack","Bill","John","Peter","Mandy", "Richard");
+/*$PeopleList = array("Jack","Bill","John","Peter","Mandy", "Richard");
 foreach ($PeopleList as $person) {
      echo ("Inserting a new row into table" . PHP_EOL);
      $tsql= "INSERT INTO TestSchema.Employees (Name, Location) VALUES (?,?);";
@@ -33,7 +39,7 @@ if ($getResults == FALSE or $rowsAffected == FALSE)
 echo ($rowsAffected. " row(s) inserted: " . PHP_EOL);
 }
     
-/*sqlsrv_free_stmt($getResults);
+//sqlsrv_free_stmt($getResults);
 
 //Update Query
 
@@ -60,19 +66,40 @@ if ($getResults == FALSE or $rowsAffected == FALSE)
     die(FormatErrors(sqlsrv_errors()));
 echo ($rowsAffected. " row(s) deleted: " . PHP_EOL);
 sqlsrv_free_stmt($getResults);
+*/
 
 
 //Read Query
-$tsql= "SELECT Id, Name, Location FROM TestSchema.Employees;";
+$tsql= "SELECT        customers.customer_name, customers.customer_abreviation, customer_engagement_teams.first_name, customer_engagement_teams.last_name, customer_engagement_teams.designation, 
+customer_engagement_teams.designation_short, customer_engagement_teams.mobile_number, customer_engagement_teams.email_adress, 
+customer_engagement_teams.include_in_escalations
+FROM            customers INNER JOIN
+customer_team_link ON customers.customer_id = customer_team_link.customer_id INNER JOIN
+customer_engagement_teams ON customer_team_link.team_member_id = customer_engagement_teams.team_member_id";
+
 $getResults= sqlsrv_query($conn, $tsql);
 echo ("Reading data from table" . PHP_EOL);
 if ($getResults == FALSE)
     die(FormatErrors(sqlsrv_errors()));
+    echo '<table border = "1" cellpadding = "5" cellspacing = "5" style="width:100%">';
+    echo "<tr><th>Customer</th><th>Customer Abreviation</th><th>Team Member</th><th>Title</th><th>Designation</th><th>Mobile number</th><th>Email Adress</th></tr>";
+
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-    echo ($row['Id'] . " " . $row['Name'] . " " . $row['Location'] . PHP_EOL);
+    $customer = $row['customer_name'];
+    $customer_abreviation = $row['customer_abreviation'];
+    $team_member = $row['first_name'] ." " . $row['last_name'];
+    $designation = $row['designation'];
+    $designation_short = $row['designation_short'];
+    $mobile_number = $row['mobile_number'];
+    $email_adress = $row['email_adress'];
+
+    echo "<tr><td>".$customer."</td><td>".$customer_abreviation."</td><td>".$team_member."</td><td>".$designation."</td><td>".$designation_short."</td><td>".$mobile_number."</td><td>".$email_adress."</td></tr>";
+
+    //echo ($row['customer_name'] . " | " . $row['customer_abreviation'] . " | " . $row['first_name'] ." " . $row['last_name'] . " | " . $row['designation'] . " | " . $row['designation_short'] . PHP_EOL);
 
 }
-sqlsrv_free_stmt($getResults);*/
+echo "</table>";
+//sqlsrv_free_stmt($getResults);*/
 
 function FormatErrors( $errors )
 {
